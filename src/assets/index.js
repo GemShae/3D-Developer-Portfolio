712d0f7b1a9e77199654d0770d23ada7b8d9c6cc
Shanya Connell <shanyaconnell@gmail.com>
2023-03-07 21:08:51 -0500

    modified assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,62 +1,56 @@
-import logo from "./logo.svg";
-import backend from "./backend.png";
-import creator from "./creator.png";
+import logo from "./logo.jpg";
+import ai from "./ai.png";
+import data from "./data.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
+
+import close from "./close.svg";
 import github from "./github.png";
 import menu from "./menu.svg";
-import close from "./close.svg";
+
+import ecommerce from "./ecommerce.png";
+import gemshae from "./gemshae.png";
+import truth from "./truth.jpeg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
+import java from "./tech/java.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
+import mysql from "./tech/mysql.png";
 import nodejs from "./tech/nodejs.png";
+import python from "./tech/python.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import harrison from "./company/harrison.jpeg";
+import ncb from "./company/ncb.png";
+import reggae from "./company/reggae.jpg";
 
 export {
   logo,
-  backend,
-  creator,
+  ai,
+  data,
   mobile,
   web,
+  close,
   github,
   menu,
-  close,
+  ecommerce,
+  gemshae,
+  truth,
   css,
-  docker,
-  figma,
   git,
   html,
+  java,
   javascript,
-  mongodb,
+  mysql,
   nodejs,
+  python,
   reactjs,
   redux,
   tailwind,
-  typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
 };
