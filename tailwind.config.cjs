71147c0764feb52124c6e2722f78f38b0c3da656
Shanya Connell <shanyaconnell@gmail.com>
2023-03-08 19:21:06 -0500

    adjusted background and associated colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#0b0c10",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        primary: "#000000",
+        secondary: "#8acdde",
+        tertiary: "#010c2c",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/herobg.jpg')",
       },
     },
   },
