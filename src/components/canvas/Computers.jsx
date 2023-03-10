62f5cda29b7c53554ad5abb48ab6fa9c4dd9f83f
Shanya Connell <shanyaconnell@gmail.com>
2023-03-10 17:18:00 -0500

    feat: adjusted model for mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf'); //Import the computer object
 
   return (
@@ -24,8 +24,8 @@ const Computers = () => {
       //Primitive object
       <primitive 
         object={computer.scene} 
-        scale={1.75} //How large the object is
-        position={[0, -9.25, -1.5]} //x,y,z coordinates
+        scale={ isMobile ? 1.25 : 1.75 } //How large the object is
+        position={ isMobile ? [0, -9, -2.2] : [0, -9.25, -1.5]} //x,y,z coordinates
         rotation={[-0.01, -0.2, -0.1]} 
       />  
     </mesh>
@@ -33,6 +33,23 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  //Check if the browser is mobile and set the state
+  //Adding a listener to the window resize event
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)'); //Check if the screen is mobile
+
+    setIsMobile(mediaQuery.matches); //If the screen is mobile set the state to true
+
+    const handleMediaQueryChange = (event) => {setIsMobile(event.matches);} //Callback function to handle media query change
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange); //Add the callback function as a listener to the media query change
+
+    //Have to remove the listener when the component unmounts because we're in the useEffect
+    return () => { mediaQuery.removeEventListener('change', handleMediaQueryChange); }
+  }, [])
+
   return (
     <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fav: 25 }} gl={{ preserveDrawingBuffer: true }} >
       <Suspense fallback={<CanvasLoader  /> } >
@@ -40,7 +57,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI  / 2} /* Allows you to only rotate it around a specific angle */
           minPolarAngle={Math.PI  / 2} 
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />  
