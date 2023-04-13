7bf27e4410930eb698f01ef7843bda1d0bf1e97b
Shanya Connell <shanyaconnell@gmail.com>
2023-04-12 20:46:56 -0500

    feat: updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
+    <section className={`relative w-full h-screen mx-auto`}>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#0495c8]' />  
@@ -14,8 +14,9 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#0495c8]'>Shanya</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am a results-oriented software engineer from Jamaica who is passionate about 
-            developing efficient and high-quality solutions that make a difference in people's lives by meeting real-world needs.</p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am a results-oriented software engineer from Jamaica <br className='sm:block hidden'  /> 
+            who is passionate about developing efficient and high-quality solutions that make a difference in people's lives by meeting real-world needs.
+          </p>
         </div>
       </div>
 
